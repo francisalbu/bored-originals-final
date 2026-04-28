fd57ffbdf4d72f05e36cd0388357d2c26cfbd66d
Francisco Albuquerque <francisalbu@gmail.com>
2026-04-28 16:42:27 +0100

    analytics: posthog autocapture + heatmaps + session recording + person_profiles always

--- src/main.tsx
+++ src/main.tsx
@@ -10,9 +10,16 @@ inject();
 
 posthog.init(import.meta.env.VITE_POSTHOG_KEY ?? '', {
   api_host: import.meta.env.VITE_POSTHOG_HOST ?? 'https://eu.i.posthog.com',
-  person_profiles: 'identified_only',
-  capture_pageview: true,
-  capture_pageleave: true,
+  person_profiles: 'always',          // rastreia utilizadores anónimos e identificados
+  capture_pageview: true,             // pageviews automáticos
+  capture_pageleave: true,            // saídas de página
+  autocapture: true,                  // captura todos os cliques, inputs e submissões
+  capture_heatmaps: true,             // heatmaps de cliques
+  session_recording: {
+    maskAllInputs: false,             // grava inputs (exceto passwords que são sempre mascaradas)
+    maskInputOptions: { password: true },
+  },
+  enable_heatmaps: true,
 });
 
 createRoot(document.getElementById('root')!).render(
